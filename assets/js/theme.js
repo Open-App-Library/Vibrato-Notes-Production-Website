e662e06ba4679da6b4373d2982c79b080eae2e22
Doug <contact@dougie.io>
2018-09-25 22:35:18 -0400

    Removed horrible preloader made by satan

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -14,16 +14,6 @@
 (function($) {
     "use strict";
 
-
-
-    /*---------------------
-    preloader
-    --------------------- */
-
-    $(window).on('load', function() {
-        $('#preloader').delay(1000).fadeOut('slow', function() { $(this).remove(); });
-    });
-
     //=========================
     //  Active current menu while scrolling
     //=========================
